e852452a58673468c97c3aa3f25558883de535b3
ZSWA <znuri160@gmail.com>
2020-10-24 22:16:49 +0700

    Soal

---
+++ Praktikum 5/Praktikum chapter 1/If else.py
@@ -0,0 +1,18 @@
+bil = 10
+if (bil>0) :
+    print("Bilangan positif")
+
+bil2 = -4
+if (bil2>0) :
+    print("Bilangan positif")
+else :
+    print("Bilangan negatif atau nol")
+
+bil0 = 0
+if (bil0>0) :
+    print("Bilangan positif")
+elif (bil0<0) :
+    print("Bilangan negatif")
+else :
+    print("Bilangan nol")
+
