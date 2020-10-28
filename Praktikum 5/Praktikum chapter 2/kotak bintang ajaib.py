b4b662eaf5bde399d877eb8d1f4626167544d965
ZSWA <znuri160@gmail.com>
2020-10-28 13:13:39 +0700

    praktikum chapter 2

---
+++ Praktikum 5/Praktikum chapter 2/kotak bintang ajaib.py
@@ -0,0 +1,13 @@
+kolom =  5
+baris = 5
+
+i=0
+
+while(i<baris) :
+    j =i+1
+    while (j>0) :
+        print("*", end="")
+        j-=1
+    print("")
+    i+=1
+    
---
+++ Praktikum 5/Praktikum chapter 2/nomor 11.py
@@ -0,0 +1,10 @@
+from random import randint
+jml=1
+while True :
+    bil = randint(0,10)
+    print(bil)
+    if bil == 5 :
+        
+        break    
+    jml+=1
+print("Jumlah Perulangan :", a)
