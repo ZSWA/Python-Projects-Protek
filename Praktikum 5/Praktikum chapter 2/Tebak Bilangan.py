7eb1bba3e482d37fedd880fb564a7f0453a8ca69
ZSWA <znuri160@gmail.com>
2020-10-30 14:42:08 +0700

    Tebak Angka

---
+++ Praktikum 5/Praktikum chapter 2/Tebak Bilangan.py
@@ -0,0 +1,35 @@
+from random import randint
+import time
+
+print( "Hai, Selamat datang di Permainan Tebak Angka, Coba tebak angka yang saya simpan ^-^)/")
+score = 100
+print ("Score Awal Anda = ", score)
+Tebak = randint(1, 100)
+print(Tebak)
+time.sleep(2)
+print("Sedang mendapatkan angka...")
+
+time.sleep(2)
+print("Angka sudah didapatkan, sekarang tebak angkanya!")
+
+time.sleep(1)
+Jawab = int(input("Masukkan Jawaban Anda : "))
+if (score<0) :
+    print("Game Over")
+else :           
+    while (Jawab<Tebak) :
+        print ("Jawaban anda terlalu kecil")
+        print ("---------------------------------------")
+        Jawab = int(input("Masukkan Jawaban Anda : "))
+        score-=2
+
+    while (Jawab>Tebak) :
+        print ("Jawaban anda terlalu Besar")
+        print ("---------------------------------------")
+        Jawab = int(input("Masukkan Jawaban Anda : "))
+        score-=2
+
+    if (Jawab == Tebak) :
+        print ("---------------------------------------")
+        print("Selamat Jawaban anda Benar ^-^)/")
+        print("Score anda = ", score)
