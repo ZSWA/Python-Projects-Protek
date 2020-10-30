f1ad4c9dde93d3495f928552d190f18a891033eb
ZSWA <znuri160@gmail.com>
2020-10-30 14:12:16 +0700

    30/10/2020

--- Praktikum 5/Praktikum chapter 2/perulangan ganjil.py
+++ Praktikum 5/Praktikum chapter 2/perulangan ganjil.py
@@ -1,5 +1,16 @@
 ganjil = 0
 ganjil+=1
+jumlah = 1
+a=1
 while (ganjil<100) :
-    print(ganjil)
-    ganjil+=2
+    if ganjil == 99 :
+        print(ganjil)
+        break
+    else :
+        print(ganjil)
+        ganjil+=2
+        jumlah += ganjil
+        a+=1
+
+print("banyaknya bilangan ganjil = ",a)
+print("Jumlah seluruh bilangan ganjil" ,jumlah)
