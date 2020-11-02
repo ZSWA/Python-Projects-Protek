59ecf3978504a95ab4bdce509f6a43bbc838d3b3
ZSWA <znuri160@gmail.com>
2020-11-02 09:15:33 +0700

    Operasi Matematika

---
+++ Praktikum 6/operation.py
@@ -0,0 +1,17 @@
+
+
+def jumlah(a,b) :
+    hasil = a+b
+    return hasil
+
+def kali(a,b)  :
+    hasil = a*b
+    return hasil
+
+def kurang(a,b) :
+    hasil = a-b
+    return hasil
+
+def bagi(a,b) :
+    hasil = a/b
+    return hasil
