98d6717007f4b7dbd6004e53f402d812e36699ea
ZSWA <znuri160@gmail.com>
2020-11-02 09:14:58 +0700

    Luas Segitiga

---
+++ Praktikum 6/Praktikum 2.py
@@ -0,0 +1,23 @@
+def luasSegitiga(a,t) :
+    luas = a*t/2
+    return luas
+
+alas = 10
+tinggi = 20
+print("Luas segitiga dengan alas", alas, "dan tinggi", tinggi, "adalah", luasSegitiga(alas,tinggi))
+
+alas2 = 15
+tinggi2 = 45
+print("Luas segitiga dengan alas", alas, "dan tinggi", tinggi, "adalah", luasSegitiga(alas2,tinggi2))
+
+def luasSegitiga2(a,t) :
+    luas = a*t/2
+    print("Luas segitiga dengan alas", a, "dan tinggi", t, "adalah", luas)
+
+alas = 10
+tinggi = 20
+luasSegitiga2(alas,tinggi)
+
+alas2 = 15
+tinggi2 = 45
+luasSegitiga2(alas2,tinggi2)
